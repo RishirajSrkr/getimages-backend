559b08c5ed1e2635ee0babba3c3906e58d5f1167
RishirajSrkr <113599778+RishirajSrkr@users.noreply.github.com>
2024-03-28 19:34:45 +0530

    final i guess??

--- dbConnection/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('MongoDB Connnected...')
-    }).catch((err) => {
-        console.log('Error while Mongo Conn..', err);
-    })
